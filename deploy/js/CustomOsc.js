2d302d59d3628ee68353dbe1396fa47bd75c952a
Christian Persson <christianperss@gmail.com>
2013-12-15 22:01:43 +0000

    sdf

--- deploy/js/CustomOsc.js
+++ deploy/js/CustomOsc.js
@@ -5,39 +5,111 @@
 		this._audioCtx = null;
 		this._oscNode = null;
 		this._gainNode = null;
+		this._el = null;
+		this._visualControllers = {freq: 
+								{
+									valEl: null,
+									blockEl: null,
+									slider: null
+								},
+								gain: {
+									valEl: null,
+									blockEl: null,
+									slider: null
+								}
+							
+							};
+
+		this._freqSlider = null;
+		this._gainSlider = null;
+
+		this._gainVol = undefined;
 	};
 
-	var p = Modulator.prototype;
+	var p = CustomOsc.prototype;
 
-	p.setup = function(ctx, type, freq, gain){
+	p.setup = function(ctx, type, freq, vol, envObj, el, mul){
 
+		this._el = el;
+
+		this._mul = mul;
+		
+		this._visualControllers.freq.blockEl = this._el.querySelector('.inputBlock.freq');
+		this._visualControllers.freq.valEl = this._visualControllers.freq.blockEl.querySelector('.val');
+		this._visualControllers.freq.slider = this._visualControllers.freq.blockEl.querySelector('input');
+		this._visualControllers.freq.slider.value = freq;
+		this._visualControllers.freq.slider.addEventListener('change', this.onFreqSliderChange.bind(this));
+
+		var attackSlider = this._el.querySelector('.inputBlock.envelope .attack input');
+		attackSlider.addEventListener('change', this.onAttackSliderChange.bind(this));
+	
+	
 		this._audioCtx = ctx;
 		this._oscNode = this._audioCtx.createOscillator();
-		this._gainNode = this._audioCtx.createGainNode();
-		this.setGain(gain);
+		// this._gainNode = this._audioCtx.createGainNode();
+		this._envelope = new Envelope();
+		this._envelope.setup(ctx, this._el.querySelector('.inputBlock.envelope'), envObj);
+
+		// this.setGain(gain);
+		this._gainVol = vol;
 		this._oscNode.type = type;
 		this.setFreq(freq);
-		this._oscNode.connect(this._gainNode);
-		this.start();
-		
+		this._oscNode.connect(this._envelope.node);
+		this._oscNode.start(0);
+
 	};
 
-	p.start = function(){
 
-		this._oscNode.start(0);	
+
+	
+
+	p.computeFreqMul = function(freq){
+
+		return freq * this._mul / 4;	
 	};
 
 	p.setGain = function(gain){
 
-		this._gainNode.gain.value = gain;	
+		// this._gainNode.gain.value = gain;
+		// this._visualControllers.gain.valEl.innerHTML = gain;
 	};
+
+	
 
 	p.setFreq = function(freq){
 
-		this._oscNode.frequency.value = freq;
+		var mulFreq = this.computeFreqMul(freq);
+
+		this._oscNode.frequency.value = mulFreq;
+		this._visualControllers.freq.valEl.innerHTML = mulFreq;
 	};
 
+	p.getOscNode = function(){
+
+		return this._oscNode;	
+	};
+
+	p.getGainNode = function(){
+
+		return this._envelope.node;
+	};
+
+	p.onFreqSliderChange = function(e){
+
+		var val = e.target.value;
+		this.setFreq(val);
+	};
+
+	p.onAttackSliderChange = function(e){
 
 
+
+	};
+
+	// p.onGainSliderChange = function(e){
+
+	// 	var val = e.target.value;
+	// 	this.setGain(val);
+	// };
 
 })();
---
+++ deploy/js/Envelope.js
@@ -0,0 +1,95 @@
+(function(){
+
+	window.Envelope = function(){
+
+		this._audioCtx = null;
+		this.node = null;
+		this._el = null;
+		this._attack = undefined;
+		this._decay = undefined;
+		this._sustain = undefined;
+		this._release = undefined;
+
+		this._visualControllers = {
+			attack : {
+				valEl : null,
+				blockEl: null,
+				slider: null
+			},
+			decay : {
+				valEl : null,
+				blockEl: null,
+				slider: null
+			},
+			sustain : {
+				valEl : null,
+				blockEl: null,
+				slider: null
+			},
+			release : {
+				valEl : null,
+				blockEl: null,
+				slider: null
+			}
+		}
+
+	};
+
+	var p = Envelope.prototype;
+
+	p.setup = function(ctx, el, initVals){
+
+
+		this._audioCtx = ctx;
+		this._el = el;
+
+		this.node = ctx.createGain();
+
+		this._visualControllers.attack.blockEl = this._el.querySelector('.attack');
+		this._visualControllers.attack.valEl = this._visualControllers.attack.blockEl.querySelector('.val');
+		this._visualControllers.attack.slider = this._visualControllers.attack.blockEl.querySelector('input');
+		this._visualControllers.attack.slider.value = initVals.att;
+
+		this._visualControllers.decay.blockEl = this._el.querySelector('.decay');
+		this._visualControllers.decay.valEl = this._visualControllers.decay.blockEl.querySelector('.val');
+		this._visualControllers.decay.slider = this._visualControllers.decay.blockEl.querySelector('input');
+		this._visualControllers.decay.slider.value = initVals.dec;
+
+		this._visualControllers.sustain.blockEl = this._el.querySelector('.sustain');
+		this._visualControllers.sustain.valEl = this._visualControllers.sustain.blockEl.querySelector('.val');
+		this._visualControllers.sustain.slider = this._visualControllers.sustain.blockEl.querySelector('input');
+		this._visualControllers.sustain.slider.value = initVals.sus;
+
+		this._visualControllers.release.blockEl = this._el.querySelector('.release');
+		this._visualControllers.release.valEl = this._visualControllers.release.blockEl.querySelector('.val');
+		this._visualControllers.release.slider = this._visualControllers.release.blockEl.querySelector('input');
+		this._visualControllers.release.slider.value = initVals.rel;
+		
+		this._attack = initVals.att/1000;
+		this._decay = initVals.dec/1000;
+		this._sustain = initVals.sus;
+		this._release = initVals.rel/1000;
+	};
+
+	p.trigger = function(time, volume){
+
+		// console.log('env trigger', time, volume);
+
+		this.node.gain.cancelScheduledValues(time);
+		this.node.gain.value = volume;
+		this.node.gain.setValueAtTime(0, time);
+		this.node.gain.linearRampToValueAtTime(volume, time + this._attack);
+		this.node.gain.linearRampToValueAtTime(volume * this._sustain, time + this._attack + this._decay);
+	};
+
+	p.release = function(time){
+
+		// console.log('env release', time);
+
+		this.node.gain.cancelScheduledValues(0);
+		this.node.gain.setValueAtTime(this.node.gain.value, time);
+		this.node.gain.linearRampToValueAtTime(0, time + this._release);
+	};
+
+
+})();
